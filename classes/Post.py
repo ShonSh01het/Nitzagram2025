55aa7fe17516edf692a151fe7fccd03a9d6fb112
User <anime2007gm@gmail.com>
2025-02-21 23:03:47 +0200

    gleb_part_not_complited

--- classes/Post.py
+++ classes/Post.py
@@ -4,13 +4,18 @@ from constants import *
 from helpers import screen
 
 
+
 class Post:
     """
     A class used to represent post on Nitzagram
     """
-    def __init__(self): #TODO: add parameters
-        #TODO: write me!
-        pass
+
+    def __init__(self, user_name, location, likes_counter, comments, description):
+        self.user_name = user_name
+        self.location = location
+        self.likes_counter = likes_counter
+        self.comments = comments
+        self.description = description
 
     def display(self):
         """
@@ -19,8 +24,56 @@ class Post:
 
         :return: None
         """
-        # TODO: write me!
-        pass
+        myfont = pygame.font.Font('fonts/Roboto_Condensed-Black.ttf', 15)
+        write_user = myfont.render(str(self.user_name), True, BLACK)
+        screen.blit(write_user, (USER_NAME_X_POS, USER_NAME_Y_POS))
+        write_location = myfont.render(str(self.location), True, LIGHT_GRAY)
+        screen.blit(write_location, (USER_NAME_X_POS, USER_NAME_Y_POS + 15))
+        write_likes_counter = myfont.render("Liked by " + str(self.likes_counter) + " users", True, GREY)
+        screen.blit(write_likes_counter, (LIKE_TEXT_X_POS, LIKE_TEXT_Y_POS))
+        write_description = myfont.render(str(self.description), True, BLACK)
+        screen.blit(write_description, (DESCRIPTION_TEXT_X_POS, DESCRIPTION_TEXT_Y_POS))
+        COMMENT_X_POS = FIRST_COMMENT_X_POS
+        COMMENT_Y_POS = FIRST_COMMENT_Y_POS
+        if len(self.comments) >= NUM_OF_COMMENTS_TO_DISPLAY:
+            for comment in range(NUM_OF_COMMENTS_TO_DISPLAY):
+                write_comment = myfont.render(str(self.comments[comment]), True, GREY)
+                screen.blit(write_comment, (COMMENT_X_POS, COMMENT_Y_POS))
+                COMMENT_Y_POS += 15
+        else:
+            for comment in range(len(self.comments)):
+                write_comment = myfont.render(str(self.comments[comment]), True, GREY)
+                screen.blit(write_comment, (COMMENT_X_POS, COMMENT_Y_POS))
+                COMMENT_Y_POS += 15
+
+
+class Image_Post(Post):
+    def __init__(self, image, user_name, location, likes_counter, comments, description):
+        super().__init__(user_name, location, likes_counter, comments, description)
+        self.image = image
+
+    def display(self):
+        super().display()
+        post_image = pygame.image.load(self.image)
+        small_image = pygame.transform.smoothscale(post_image, (POST_WIDTH, POST_HEIGHT))
+        screen.blit(small_image, (POST_X_POS, POST_Y_POS))
+
+
+class Text_Post(Post):
+    def __init__(self, text, color, backgraund_color, user_name, location, likes_counter, comments, description):
+        super().__init__(user_name, location, likes_counter, comments, description)
+        self.text = text
+        self.color = color
+        self.backgraund_color = backgraund_color
+
+    def display(self):
+        super().display()
+        backgraund = pygame.Surface((POST_WIDTH, POST_HEIGHT))
+        backgraund.fill(self.backgraund_color)
+        screen.blit(backgraund, (POST_X_POS, POST_Y_POS))
+        myfont = pygame.font.Font('fonts/Roboto_Condensed-Black.ttf', TEXT_POST_FONT_SIZE)
+        write_post = myfont.render(str(self.text), True, self.color, self.backgraund_color)
+        screen.blit(write_post, (POST_X_POS, POST_Y_POS))
 
 
     def display_comments(self):
@@ -48,6 +101,5 @@ class Post:
             position_index += 1
             if i >= NUM_OF_COMMENTS_TO_DISPLAY - 1:
                 break
-
 
 
--- main.py
+++ main.py
@@ -1,6 +1,78 @@
 import pygame
 from helpers import screen
-from constants import WINDOW_WIDTH, WINDOW_HEIGHT, BLACK, GREY, WHITE
+from constants import *
+
+
+class Post:
+    """
+    A class used to represent post on Nitzagram
+    """
+
+    def __init__(self, user_name, location, likes_counter, comments, description):
+        self.user_name = user_name
+        self.location = location
+        self.likes_counter = likes_counter
+        self.comments = comments
+        self.description = description
+
+    def display(self):
+        """
+        Display the Post image/Text, description, location, likes and comments
+        on screen
+
+        :return: None
+        """
+        myfont = pygame.font.Font('fonts/Roboto_Condensed-Black.ttf', 15)
+        write_user = myfont.render(str(self.user_name), True, BLACK)
+        screen.blit(write_user, (USER_NAME_X_POS, USER_NAME_Y_POS))
+        write_location = myfont.render(str(self.location), True, LIGHT_GRAY)
+        screen.blit(write_location, (USER_NAME_X_POS, USER_NAME_Y_POS + 15))
+        write_likes_counter = myfont.render("Liked by " + str(self.likes_counter) + " users", True, GREY)
+        screen.blit(write_likes_counter, (LIKE_TEXT_X_POS, LIKE_TEXT_Y_POS))
+        write_description = myfont.render(str(self.description), True, BLACK)
+        screen.blit(write_description, (DESCRIPTION_TEXT_X_POS, DESCRIPTION_TEXT_Y_POS))
+        COMMENT_X_POS = FIRST_COMMENT_X_POS
+        COMMENT_Y_POS = FIRST_COMMENT_Y_POS
+        if len(self.comments) >= NUM_OF_COMMENTS_TO_DISPLAY:
+            for comment in range(NUM_OF_COMMENTS_TO_DISPLAY):
+                write_comment = myfont.render(str(self.comments[comment]), True, GREY)
+                screen.blit(write_comment, (COMMENT_X_POS, COMMENT_Y_POS))
+                COMMENT_Y_POS += 15
+        else:
+            for comment in range(len(self.comments)):
+                write_comment = myfont.render(str(self.comments[comment]), True, GREY)
+                screen.blit(write_comment, (COMMENT_X_POS, COMMENT_Y_POS))
+                COMMENT_Y_POS += 15
+
+
+class Image_Post(Post):
+    def __init__(self, image, user_name, location, likes_counter, comments, description):
+        super().__init__(user_name, location, likes_counter, comments, description)
+        self.image = image
+
+    def display(self):
+        super().display()
+        post_image = pygame.image.load(self.image)
+        small_image = pygame.transform.smoothscale(post_image, (POST_WIDTH, POST_HEIGHT))
+        screen.blit(small_image, (POST_X_POS, POST_Y_POS))
+
+
+class Text_Post(Post):
+    def __init__(self, text, color, backgraund_color, user_name, location, likes_counter, comments, description):
+        super().__init__(user_name, location, likes_counter, comments, description)
+        self.text = text
+        self.color = color
+        self.backgraund_color = backgraund_color
+
+    def display(self):
+        super().display()
+        backgraund = pygame.Surface((POST_WIDTH, POST_HEIGHT))
+        backgraund.fill(self.backgraund_color)
+        screen.blit(backgraund, (POST_X_POS, POST_Y_POS))
+        myfont = pygame.font.Font('fonts/Roboto_Condensed-Black.ttf', TEXT_POST_FONT_SIZE)
+        write_post = myfont.render(str(self.text), True, self.color, self.backgraund_color)
+        screen.blit(write_post, (POST_X_POS, POST_Y_POS))
+
 
 
 def main():
@@ -17,8 +89,6 @@ def main():
     background = pygame.transform.scale(background,
                                         (WINDOW_WIDTH, WINDOW_HEIGHT))
 
-    # TODO: add a post here
-
     running = True
     while running:
         # Grabs events such as key pressed, mouse pressed and so.
@@ -28,9 +98,10 @@ def main():
                 running = False
 
         # Display the background, presented Image, likes, comments, tags and location(on the Image)
+        test = Text_Post('Hello every one', 'Red', 'Green', 'gleb', 'israel', 5, ["it is true", "Really"], 'i hate this project')
         screen.fill(BLACK)
         screen.blit(background, (0, 0))
-        pygame.draw.rect(screen, (0, 0, 0), (26, 550, 310, 40), 1, 20)
+        test.display()
         # pygame.draw.rect(screen,(255,255,255), (26, 551, 308, 38))
         # Update display - without input update everything
         pygame.display.update()
